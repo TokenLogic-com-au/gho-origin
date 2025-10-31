8c916222d63ef9135f9813c43afef8d334e02e14
miguelmtzinf <miguelmtz.mail@gmail.com>
2025-10-31 13:39:10 +0100

    fix: lint

--- src/contracts/facilitators/gsm/swapFreezer/interfaces/IGelatoOracleSwapFreezer.sol
+++ src/contracts/facilitators/gsm/swapFreezer/interfaces/IGelatoOracleSwapFreezer.sol
@@ -3,5 +3,6 @@ pragma solidity ^0.8.0;
 
 interface IGelatoOracleSwapFreezer {
   function performUpkeep(bytes calldata) external;
+
   function checkUpkeep(bytes calldata) external view returns (bool, bytes memory);
 }
